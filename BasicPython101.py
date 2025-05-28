bcb01a5e936b29a45a71f98897c26640f215c892
Weeraw1ch-git <weer4w1ch@gmail.com>
2025-05-28 18:03:05 +0700

    Add files via upload

--- BasicPython101.py
+++ BasicPython101.py
@@ -1,20 +1,32 @@
 #lower 18 do not entry !!!
-while True :
+
+
+name = str(input("Enter your name : "))
+day,month,year= int(input("Day : ")) , input("Month : ") , int(input("Years: "))
+
+
+def noYounger20():
+
+    while True :
     
-    name = str(input("Enter your name : "))
-    print("Enter Your birth")
-    day,month,year= int(input("Day : ")) , input("Month : ") , int(input("Years: "))
-    if year <= 2025 :
-        break
-    elif year > 2025 :
-        print("ERROR!")
+        #name = str(input("Enter your name : "))
+        #print("Enter Your birth")
+        #day,month,year= int(input("Day : ")) , input("Month : ") , int(input("Years: "))
+        if year <= 2025 :
+            break
+        elif year > 2025 :
+            print("ERROR!")
 
-print(f"Your birth day is : {day} {month} {year}")
+    print(f"Your birth day is : {day} {month} {year}")
 
-if 2025 - year >= 20 :
-    print(f"Welcome {name} ")
-elif 2025 - year < 20 :
-    print(f"{name} not allowed to enter.")    
-else :
-    print("error")
-        
+    if 2025 - year >= 20 :
+      print(f"Welcome {name} ")
+    elif 2025 - year < 20 :
+        print(f"{name} not allowed to enter.")    
+    else :
+        print("error")
+
+noYounger20()
+
+
+    
